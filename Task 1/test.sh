22dd3d5a30589e3e05fe2bad9646789c2dd1c806
Dmitry <godrik2003@mail.ru>
2024-04-04 14:42:08 +0300

    The format of the input data for the tests has been changed
    The input data format has become more convenient for creating new tests. New tests have also been added.

--- Task 1/test.sh
+++ Task 1/test.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
+format_test_file() {
+    line_count_first=0
+
+    while IFS= read -r line || [[ -n "$line" ]]; do
+        if [[ -z "$line" ]]; then
+            break
+        fi
+        ((line_count_first++))
+    done < "tests/$1.txt"
+
+    line_count_second=$(awk 'END{print NR}' "tests/$1.txt")
+    line_count_second=$((line_count_second - line_count_first - 1))
+
+    columns=$(awk '{print NF}' "tests/$1.txt" | sort -nu | tail -n 1)
+
+    echo "$line_count_first" > "tests/temp.txt"
+    awk -v columns="$columns" -v line_count_second="$line_count_second" '{
+        if (NF == 0) {
+            printf "%d\n", line_count_second;
+        } else {
+            printf "%d\n", NF; 
+            for(i=1; i<=NF; i++) {
+                printf "%d\n", $i; 
+            }
+        }
+    }' "tests/$1.txt" >> "tests/temp.txt"
+}
+
 run_test() {
     echo "Running test with input file: $1"
     echo -e ' \t '
 
-    a=$(./array.exe < "tests/$1.txt")
+    format_test_file "$1"
+
+    a=$(./array.exe < "tests/temp.txt")
+    
     result="${a: -2}"
-    #echo "$result"
     if [ "$result" -eq 1 ]; then
         echo -e "Test $1 \033[102mpassed\033[0m"
     else
@@ -16,9 +46,11 @@ run_test() {
     echo -e ' \t '
 }
 
-input=("Usual_case" "Example_1" "Example_2" "Example_3" "All_Null"
-"Duplicate_elements")
+input=("Usual_case" "Example_1" "Example_2_fail" "Example_3_fail" "All_Null"
+"Duplicate_elements" "Letter_array" "Double_array")
 
 for input_file in "${input[@]}"; do
-    run_test "$input_file"
+   run_test "$input_file"
 done
+
+rm "tests/temp.txt"
