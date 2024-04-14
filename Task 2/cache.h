b844deacb72e4134aa2ab57840bfa6f666155f27
Dmitry <godrik2003@mail.ru>
2024-04-14 22:47:45 +0300

    The first version of the suspension The ability to write to a suspension file for one number, minimizing two buffers

--- Task 2/cache.h
+++ Task 2/cache.h
@@ -18,9 +18,21 @@ typedef struct
     Node * root;
 } BinaryTree;
 
+typedef struct 
+{
+    int height;
+    int width;
+} size;
+
 BinaryTree * binary_tree_init(int key);
 
+void buffer_size(int start, int end, int * size_x, int * size_y, BinaryTree * bd);
+
+void build_divider_tree(int number, BinaryTree * bd);
+
 Node * node_init(int key);
+
+void node_size(Node * n, int current_x, int current_y, int * max_x, int * max_y, int level);
 
 void node_delete(Node * curr);
 
@@ -34,4 +46,13 @@ Node * binary_tree_find(int key, BinaryTree * bt);
 
 void binary_tree_insert(int key, BinaryTree * bt);
 
+void buildTreeString(Node* node, int level, char* buffer, int* index);
+
+void fill_buffer_node(Node * n, char ** buffer, int current_x, int current_y, int * max_x, int * max_y, int level);
+
+void combining_buffer(char ** first_buffer, char ** second_buffer, int first_x, int first_y, int second_x, int second_y);
+
+void print_number(int number, char ** buffer, int current_x, int current_y);
+
+int number_of_digits(int x);
 #endif
--- Task 2/main.c
+++ Task 2/main.c
@@ -4,6 +4,54 @@
 
 #include "cache.h"
 
+static void print_buffer(char *buffer[], int max_Y, int max_X) {
+    char* name_file = "suspension.txt";
+    FILE* file = fopen(name_file, "w");
+    for (int i = 1; i < max_Y; i++) {
+        for(int j = 1; j < max_X; j++) {
+            fprintf(file, "%c", buffer[i][j]);
+        }
+        fprintf(file, "\n");
+    }
+    fclose(file);
+}
+
+char** create_buffer(int rows, int cols) {
+    char **buffer = (char**)malloc(rows * sizeof(char*));
+    for (int i = 0; i < rows; i++) {
+        buffer[i] = (char *)malloc(cols * sizeof(char));
+        for (int j = 0; j < cols; j++) {
+            buffer[i][j] = ' ';
+        }
+    }
+    return buffer;
+}
+
+void print_min(int first, int second, BinaryTree * cache){
+    int first_max_x = number_of_digits(first) + 1;
+    int first_max_y = 0;
+    int second_max_x = number_of_digits(second) + 1;
+    int second_max_y = 0;
+
+    int first_x = 0;
+    int first_y = 0;
+    int second_x = 0;
+    int second_y = 0;
+
+
+    node_size(binary_tree_find(first, cache), 0, 0, &first_max_x, &first_max_y, 0);
+    char **first_buffer = create_buffer(first_max_y + 2, first_max_x);
+    fill_buffer_node(binary_tree_find(first, cache), first_buffer, 1, 1, &first_x, &first_y, 0);
+
+    node_size(binary_tree_find(second, cache), 0, 0, &second_max_x, &second_max_y, 0);
+    char **second_buffer = create_buffer(second_max_y + 2, second_max_x);
+    fill_buffer_node(binary_tree_find(second, cache), second_buffer, 1, 1, &second_x, &second_y, 0);
+
+    combining_buffer(first_buffer, second_buffer, first_max_x, first_max_y + 2, second_max_x, second_max_y + 2);
+    print_buffer(first_buffer, first_max_y + 2, first_max_x);
+
+}
+
 int main(){
 
     int start, end, input;
@@ -12,11 +60,11 @@ int main(){
     printf("Enter end: ");
     scanf("%d", &end);
     
-    if (start < 0 || end < 0 || end < start)
-    {
-        printf("Incorrect input. Restart the program.\n");
-        return 0;
-    }
+    // if (start < 0 || end < 0 || end < start)
+    // {
+    //     printf("Incorrect input. Restart the program.\n");
+    //     return 0;
+    // }
     
     BinaryTree * cache = binary_tree_init(end);
 
@@ -25,35 +73,46 @@ int main(){
         build_divider_tree(i, cache);
     }
 
-    printf("Output of all divisors: [1]\nOutput of the divisors of a number: [2]\n");
-    scanf("%d", &input);
-    switch (input)
-    {
-    case 1:
-        binary_tree_print(cache);
-        break;
+    //char buffer[100];
+    print_min(1000, 1001, cache);
 
-    case 2:
-        printf("Input number: ");
-        scanf("%d", &input);
+    // Построение строки для дерева
+    // buildTreeString(binary_tree_find(10, cache), 0, buffer, &index);
+    // buffer[index] = '\0';
 
-        Node * inp = binary_tree_find(input, cache);
+    
+    // Вывод строки с деревом
+    //printf("%d\t %d", s.height, s.width);
 
-        if (inp != NULL)
-        {
-            print_node(inp);
-        }
-        else
-        {
-            printf("This number is missing");
-        }
+    // printf("Output of all divisors: [1]\nOutput of the divisors of a number: [2]\n");
+    // scanf("%d", &input);
+    // switch (input)
+    // {
+    // case 1:
+    //     binary_tree_print(cache);
+    //     break;
 
-        break;
+    // case 2:
+    //     printf("Input number: ");
+    //     scanf("%d", &input);
 
-    default:
-        printf("Incorrect input.");
-        break;
-    }
+    //     Node * inp = binary_tree_find(input, cache);
+
+    //     if (inp != NULL)
+    //     {
+    //         print_node(inp);
+    //     }
+    //     else
+    //     {
+    //         printf("This number is missing");
+    //     }
+
+    //     break;
+
+    // default:
+    //     printf("Incorrect input.");
+    //     break;
+    // }
     
     binary_tree_free(cache);
     
