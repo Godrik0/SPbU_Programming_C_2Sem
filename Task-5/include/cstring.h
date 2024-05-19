ae8a9e6c6ae68b2531c1459b8ca1079231b516da
Dmitry <godrik2003@mail.ru>
2024-05-20 02:03:39 +0300

    The task is completed

--- Task-5/include/cstring.h
+++ Task-5/include/cstring.h
@@ -11,27 +11,33 @@ typedef struct _cstring
 cstring * cstring_create(const char *s);
 cstring * cstring_init(char *s, int length);
 void cstring_delete(cstring * str);
-void cstring_insert(cstring * to, const char * from, int pos);
+void cstring_insert(cstring * to, const char * from, int pos, int from_len);
 cstring * cstring_substring(cstring * str, int sub_start, int sub_lenght);
 int cstring_find(const cstring * text, const char * pat, const int pos);
-cstring ** cstring_split(const cstring * str, const char * sepator);
+cstring ** cstring_split(const cstring * str, const char * separator);
+cstring ** cstring_split_char(const cstring * str, const char separator);
 int cstring_compare(const cstring * str1, const cstring * str2);
+int cstring_rfind(const cstring * text, const char pat);
 
-
-static inline void cstring_append(cstring *cs, const char *s)
-{
-	cstring_insert(cs, s, cs->length);
-}
 
 static inline int cstring_empty(const cstring *cs)
 {
 	return (cs->data == 0 && cs->length == 0);
 }
 
-static inline int cstring_rfind(const cstring * text, const char * pat, const int pos)
+static inline void cstring_insert_char(cstring * to, const char * from, int pos)
 {
-    int result = cstring_find(text, pat, pos) ; 
-    return result != -1 ? result + my_strlen(pat) : -1;
+    cstring_insert(to, from, pos, my_strlen(from));
+}
+
+static inline void cstring_insert_cstring(cstring * to, const cstring * from, int pos)
+{
+    cstring_insert(to, from->data, pos, from->length);
+}
+
+static inline void cstring_append(cstring *cs, const char *s)
+{
+	cstring_insert_char(cs, s, cs->length);
 }
 
 static inline void cstring_concatenation(cstring * to, const cstring * from)
